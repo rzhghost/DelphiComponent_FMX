0c235d004cd7be45d302be4fa1acb081e69c1561
rzhghost <33561703+rzhghost@users.noreply.github.com>
2020-03-11 11:59:02 +0800

    加入缩略图功能

--- uSyncWebImage.pas
+++ uSyncWebImage.pas
@@ -4,18 +4,26 @@ interface
 
 uses
   System.SysUtils, System.Classes, System.Types, FMX.Types, FMX.Controls, FMX.Objects,
-  FMX.StdCtrls, System.Threading, System.Net.HttpClient, FMX.PlatForm, FMX.MediaLibrary, System.Messaging;
+  FMX.StdCtrls, System.Threading, System.Net.HttpClient, FMX.PlatForm, FMX.MediaLibrary,
+  FMX.Graphics, System.Messaging;
 
 type
+  TOnDownLoadFinishEvent = procedure(Sender: TObject; NotifyObject:TObject; IsFinish:Boolean) of object;
+
   TSyncWebImage = class(TImage)
   private
     { Private declarations }
+    AFitWidth,
+    AFitHeight:Integer; //fit image
     FURL: string; //web url
     FClient: THTTPClient;
     ProgressBar: TProgressBar;
     FGlobalStart: Cardinal;
     FAsyncResult: IAsyncResult;
     FDownloadStream: TStream;
+    FNotifyObject:TObject;
+    FOnDownLoadFinish:TOnDownLoadFinishEvent;
+
     procedure SetURL(const Value: string);
     procedure DoEndDownload(const AsyncResult: IAsyncResult);
     procedure ReceiveDataEvent(const Sender: TObject; AContentLength,
@@ -31,6 +39,10 @@ type
   published
     { Published declarations }
     property URL:string read FURL write SetURL;
+    property FitWidth:Integer read AFitWidth write AFitWidth;
+    property FitHeight:Integer read AFitHeight write AFitHeight;
+    property NotifyObject:TObject read FNotifyObject write FNotifyObject;
+    property OnDownLoadFinish:TOnDownLoadFinishEvent read FOnDownLoadFinish write FOnDownLoadFinish;
   end;
 
 procedure Register;
@@ -48,8 +60,8 @@ constructor TSyncWebImage.Create(AOwner: TComponent);
 begin
   inherited;
   FClient := THTTPClient.Create;
-  FClient.ConnectionTimeout := 90000;
-  FClient.ResponseTimeout := 90000;
+  FClient.ConnectionTimeout := 5000;
+  FClient.ResponseTimeout := 5000;
   //以常用方式访问,有些服务器会限制非标准请求
   FClient.UserAgent := 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Safari/605.1.15';
   FClient.OnReceiveData := ReceiveDataEvent;
@@ -126,6 +138,8 @@ end;
 procedure TSyncWebImage.DoEndDownload(const AsyncResult: IAsyncResult);
 var
   LAsyncResponse: IHTTPResponse;
+  ThumbailBitmap: TBitmap;
+  scale: Single;
 begin
   try
     LAsyncResponse := THTTPClient.EndAsyncHTTP(AsyncResult);
@@ -136,6 +150,17 @@ begin
         begin
           FDownloadStream.Position := 0;
           Self.bitmap.LoadFromStream(FDownloadStream);
+          if (FitWidth<>0) and (FitHeight<>0) then
+          begin
+              scale := RectF(0, 0, Self.bitmap.Width, Self.bitmap.Height).Fit(RectF(0, 0, AFitWidth, AFitHeight));
+              ThumbailBitmap := Self.bitmap.CreateThumbnail(Round(Self.bitmap.Width / scale), Round(Self.bitmap.Height / scale));
+              try
+                Self.bitmap.Clear(0);
+                Self.bitmap.Assign(ThumbailBitmap);
+              finally
+                ThumbailBitmap.Free;
+              end;
+          end;
           ProgressBar.Visible := False;
           self.Repaint;
         end;
@@ -143,6 +168,11 @@ begin
   finally
     LAsyncResponse := nil;
     FreeandNil(FDownloadStream);
+  end;
+  //如果需要通知其它外部控件,则发出消息
+  if Assigned(FOnDownLoadFinish) then
+  begin
+    FOnDownLoadFinish(Self, FNotifyObject, True);
   end;
 end;
 
